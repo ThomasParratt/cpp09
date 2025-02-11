172e16d3fae1d827e27dd052486111738f678f07
ThomasParratt <thomasparratt@gmail.com>
2025-02-11 11:17:00 +0200

    added map

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <map>
 
 class btc
 {
     private:
         std::string _input;
+        std::map<std::string, int>  _inputMap;
     public:
         btc(std::string input);
         void    readFile();
