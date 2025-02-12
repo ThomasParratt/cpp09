c1b4630c9f4a8c23dfedb221f8d4ac3ffe71e5da
ThomasParratt <thomasparratt@gmail.com>
2025-02-12 10:24:55 +0200

    need to test map

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -10,28 +10,36 @@ void    btc::readFile()
     std::ifstream   input(_input);
     std::string     date;
     float           value;
+    std::string     line;
 
     if (!input)
     {
         std::cerr << "Error opening file" << std::endl;
         exit(1);
     }
-        
-    // while (input >> date >> value)
-    // {
-    //     _inputMap[date] = value;
-    // }
 
-    while (std::getline(input, date, '|') && (input >> value)) 
+    while (std::getline(input, line))
     {
+        std::cout << line << std::endl;
+        size_t  pos = line.find('|');
+        size_t  pos1 = line.find('\n');
+        if (pos != std::string::npos)
+        {
+            date = line.substr(0, pos - 1);
+        }
+        else
+        {
+            std::cout << "Error: Line contains no |" << std::endl;
+            continue ;
+        }
+        try
+        {
+            value = std::stof(line.substr(pos + 2, pos1));
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl; //exception for first line of file
+        }
         _inputMap[date] = value;
-    }
-
-    if (_inputMap.empty())
-        std::cout << "No data was read from file" << std::endl;
-    
-    for (auto &pair : _inputMap) 
-    {
-        std::cout << pair.first << " -> " << pair.second << std::endl;
     }
 }
