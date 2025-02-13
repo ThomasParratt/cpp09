d5e33250aee27ddbb8ed4e1de4beaa5f0ce4f0bf
ThomasParratt <thomasparratt@gmail.com>
2025-02-13 12:40:10 +0200

    added date tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -10,4 +10,10 @@ int main(int argc, char **argv)
     btc bitcoin = btc(argv[1], "data.csv");
     bitcoin.readData();
     bitcoin.printResults();
+
+    // std::cout << std::boolalpha;
+    // std::cout << "Valid: " << bitcoin.isValidDate("2024-02-29") << std::endl; // Leap year case
+    // std::cout << "Valid: " << bitcoin.isValidDate("2023-02-29") << std::endl; // Invalid non-leap year
+    // std::cout << "Valid: " << bitcoin.isValidDate("2023-04-31") << std::endl; // Invalid day for April
+    // std::cout << "Valid: " << bitcoin.isValidDate("2023-12-25") << std::endl; // Valid date
 }
