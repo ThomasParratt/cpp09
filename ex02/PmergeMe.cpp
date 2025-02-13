54edd43938bdd9a2f95da2587ea8bc22f988ae22
ThomasParratt <thomasparratt@gmail.com>
2025-02-13 16:44:51 +0200

    EOD

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -2,6 +2,25 @@
 
 PmergeMe::PmergeMe(int argc, char **argv)
 {
+    // std::vector<unsigned int> seq;
+
+    // for (int i = 1; i < argc; i++)
+    // {
+    //     try
+    //     {
+    //         seq.push_back(std::stoul(argv[i]));
+    //     }
+    //     catch(const std::exception& e)
+    //     {
+    //         std::cerr << e.what() << std::endl;
+    //     }
+    // }
+
+    // std::cout << "Sequence:" << std::endl;
+    // for (auto &it : seq)
+    //     std::cout << it << std::endl;
+
+    // SORTING INTO 2D VECTOR OF PAIRS
     std::vector<std::vector<unsigned int>>   seq;
 
     for (int i = 1; i < argc; i++)
@@ -12,7 +31,7 @@ PmergeMe::PmergeMe(int argc, char **argv)
             if (i + 1 < argc)
             {
                 unsigned int num2 = std::stoul(argv[i + 1]);
-                seq.push_back({num1, num2});
+                (num1 < num2) ? seq.push_back({num1, num2}) : seq.push_back({num2, num1});
                 i++;
             }
             else
@@ -25,14 +44,12 @@ PmergeMe::PmergeMe(int argc, char **argv)
     }
 
     std::cout << "Sequence:" << std::endl;
-        for (const auto &pair : seq) 
-        {
-            std::cout << "[";
-            for (size_t j = 0; j < pair.size(); j++) 
-            {
-                std::cout << pair[j];
-                if (j + 1 < pair.size()) std::cout << ", ";
-            }
-            std::cout << "]" << std::endl;
-        }
+    for (const auto &pair : seq) 
+    {
+        std::cout << "[";
+        for (size_t j = 0; j < pair.size(); j++) 
+            std::cout << pair[j];
+        std::cout << "] ";
+    }
+    std::cout << std::endl;
 }
