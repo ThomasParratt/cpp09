5b0a0e633e908dc36b8045c958b853a0f5d97ed1
ThomasParratt <thomasparratt@gmail.com>
2025-02-28 12:10:42 +0200

    creating sequence

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -2,54 +2,10 @@
 
 PmergeMe::PmergeMe(int argc, char **argv)
 {
-    // std::vector<unsigned int> seq;
-
-    // for (int i = 1; i < argc; i++)
-    // {
-    //     try
-    //     {
-    //         seq.push_back(std::stoul(argv[i]));
-    //     }
-    //     catch(const std::exception& e)
-    //     {
-    //         std::cerr << e.what() << std::endl;
-    //     }
-    // }
-
-    // std::cout << "Sequence:" << std::endl;
-    // for (auto &it : seq)
-    //     std::cout << it << std::endl;
-
-    // SORTING INTO 2D VECTOR OF PAIRS
-    std::vector<std::vector<unsigned int>>   seq;
-
-    for (int i = 1; i < argc; i++)
+    for (int i = 1; i < argc; i+=2)
     {
-        try
-        {
-            unsigned int num1 = std::stoul(argv[i]);
-            if (i + 1 < argc)
-            {
-                unsigned int num2 = std::stoul(argv[i + 1]);
-                (num1 < num2) ? seq.push_back({num1, num2}) : seq.push_back({num2, num1});
-                i++;
-            }
-            else
-                seq.push_back({num1});
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << std::endl;
-        }
+        _seq.push_back({std::stoul(argv[i]), std::stoul(argv[i + 1])});
+        //std::cout << argv[i] << " ";
     }
-
-    std::cout << "Sequence:" << std::endl;
-    for (const auto &pair : seq) 
-    {
-        std::cout << "[";
-        for (size_t j = 0; j < pair.size(); j++) 
-            std::cout << pair[j];
-        std::cout << "] ";
-    }
-    std::cout << std::endl;
+    //std::cout << std::endl;
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -5,7 +5,7 @@
 class PmergeMe
 {
     private:
-        std::vector<unsigned int>   _seq;
+        std::vector<std::pair<unsigned int, unsigned int>>   _seq;
     public:
         PmergeMe(int argc, char **argv);
         // 1. make vector
