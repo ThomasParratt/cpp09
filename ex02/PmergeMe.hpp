b8170fec7df9551f61ccad6ee7bd7385a5f17e13
ThomasParratt <thomasparratt@gmail.com>
2025-02-28 11:56:43 +0200

    added plan notes

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -8,4 +8,11 @@ class PmergeMe
         std::vector<unsigned int>   _seq;
     public:
         PmergeMe(int argc, char **argv);
+        // 1. make vector
+        // 2. make vector with pairs (is step 1. necessary?)
+        // 3. determine the larger of the two elements in each pair (swap if needed)
+        // 4. (recursively) sort the pairs into order based on the size of the first larger element
+
+        // 5. create "main chain" (first larger elements) and "pend" (other elements)
+        // 6. insert the "pend" elements into "main chain"
 };
