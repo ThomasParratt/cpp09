cc7fe47df38735051db5d99e5d84e1f40c585706
ThomasParratt <thomasparratt@gmail.com>
2025-03-12 12:13:27 +0200

    added extra functions

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -2,19 +2,27 @@
 
 PmergeMe::PmergeMe(int argc, char **argv)
 {
-    std::vector<std::pair<unsigned int, unsigned int>> _seq;
-
     for (int i = 1; i < argc; i+=2)
     {
         if (i + 1 < argc)
             _seq.push_back(std::make_pair(std::stoul(argv[i]), std::stoul(argv[i + 1])));
         else
+        {
+            _isOdd = true;
             _odd = std::stoul(argv[i]);
+        }
     }
+}
 
+void    PmergeMe::printPairs()
+{
     for (auto &p : _seq)
-    {
         std::cout << "First: " << p.first << ", Second: " << p.second << std::endl;
-    }
-    std::cout << "First: " << _odd << std::endl;
+    if (_isOdd)
+        std::cout << "First: " << _odd << std::endl;
+}
+
+void    PmergeMe::sortPairs()
+{
+
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -9,8 +9,11 @@ class PmergeMe
         //std::vector<unsigned int>>   _seq;
         std::vector<std::pair<unsigned int, unsigned int>> _seq;
         unsigned int    _odd;
+        bool            _isOdd = false;
     public:
         PmergeMe(int argc, char **argv);
+        void    printPairs();
+        void    sortPairs();
         // 1. make vector
         // 2. make vector with pairs (is step 1. necessary?) ***
         // 3. determine the larger of the two elements in each pair (swap if needed)
