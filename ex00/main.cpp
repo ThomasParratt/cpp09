17309205fc505b7d9afc269a6d89a5990b95c3ee
ThomasParratt <thomasparratt@gmail.com>
2025-03-26 16:31:46 +0200

    removed test comments

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -11,9 +11,4 @@ int main(int argc, char **argv)
     
     bitcoin.readData();
     bitcoin.printResults(argv[1]);
-
-    // std::cout << "Valid: " << bitcoin.isValidDate("2024-02-29") << std::endl; // Leap year case
-    // std::cout << "Valid: " << bitcoin.isValidDate("2023-02-29") << std::endl; // Invalid non-leap year
-    // std::cout << "Valid: " << bitcoin.isValidDate("2023-04-31") << std::endl; // Invalid day for April
-    // std::cout << "Valid: " << bitcoin.isValidDate("2023-12-25") << std::endl; // Valid date
 }
